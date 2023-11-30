406b806fddf3eb8ba2fc5f3f994d97dd445c8288
Denis Khaziev <denis.khaziev@gmail.com>
2023-11-30 13:14:56 +0700

    Play with profiles for Github Actions

--- src/test/java/com/stitch80/ExcelHelperBot/ExcelHelperBotApplicationTests.java
+++ src/test/java/com/stitch80/ExcelHelperBot/ExcelHelperBotApplicationTests.java
@@ -2,12 +2,14 @@ package com.stitch80.ExcelHelperBot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ExcelHelperBotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
